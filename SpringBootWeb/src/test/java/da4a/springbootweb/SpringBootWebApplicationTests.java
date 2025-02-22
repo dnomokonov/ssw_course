80fa5269b00e310d9224d3451c352665b927bdf2
dnomokonov <zmnomokon715@gmail.com>
2025-02-22 20:28:12 +0700

    Add homework #3 🛠️

---
+++ SpringBootWeb/src/test/java/da4a/springbootweb/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package da4a.springbootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
